2ec8c81c1cd39eff7a416aa3e473412bee2c4a3c
William Weiskopf <worldwidewilly@comcast.net>
2012-06-03 13:36:56 -0600

    Working on a script to handle though submissions by email.

---
+++ imap.php
@@ -0,0 +1,27 @@
+<?
+  
+$mbox = imap_open("{imap.gmail.com:993}", "ideaink@weiskopf.me", "9bYQR#gq!ka4");
+
+echo "<h1>Mailboxes</h1>";
+
+if($folders == false) {
+  echo "Call Failed";
+} else {
+  foreach($folders as $folder) {
+    echo '<p>' . $folder . '</p>';
+  }
+}
+
+echo '<h1>Headers in INBOX</h1>';
+$headers = imap_headers($mbox);
+
+if($headers == false) {
+  echo '<p>' . Call failed . '</p>';
+} else {
+  foreach($headers as $header) {
+    echo '<p>' . $header . '</p>;
+  }
+}
+
+imap_close($mbox);
+?>
