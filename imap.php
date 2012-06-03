1e2cd13678ba87ae73f6c53583d6fc9c5d0f2f78
William Weiskopf <worldwidewilly@comcast.net>
2012-06-03 13:39:46 -0600

    Quote correction

--- imap.php
+++ imap.php
@@ -5,7 +5,7 @@ $mbox = imap_open("{imap.gmail.com:993}", "ideaink@weiskopf.me", "9bYQR#gq!ka4")
 echo "<h1>Mailboxes</h1>";
 
 if($folders == false) {
-  echo "Call Failed";
+  echo '<p>Call Failed</p>';
 } else {
   foreach($folders as $folder) {
     echo '<p>' . $folder . '</p>';
@@ -16,7 +16,7 @@ echo '<h1>Headers in INBOX</h1>';
 $headers = imap_headers($mbox);
 
 if($headers == false) {
-  echo '<p>' . Call failed . '</p>';
+  echo '<p>Call failed</p>';
 } else {
   foreach($headers as $header) {
     echo '<p>' . $header . '</p>;
